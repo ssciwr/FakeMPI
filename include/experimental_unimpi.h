c922dd9c7f2437ea48e93b859e888102d650d0a5
Michel Zou <xantares09@hotmail.com>
2023-03-30 08:06:46 +0200

    Add MPI_Get_address

--- include/experimental_unimpi.h
+++ include/experimental_unimpi.h
@@ -13,6 +13,9 @@ extern "C"
 #define MPI_MAX_INFO_KEY 2056
 #define MPI_MAX_INFO_VAL 2056
 
+#define MPI_Get_address(location, address)                                     \
+  (*(address) = (MPI_Aint)((char*)(location)), MPI_SUCCESS)
+
   typedef void MPI_Comm_errhandler_function(MPI_Comm*, int*, ...);
   int MPI_Comm_create_errhandler(
     MPI_Comm_errhandler_function* comm_errhandler_fn,
